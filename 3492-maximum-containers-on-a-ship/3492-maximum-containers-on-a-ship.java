6335d870c25521efce294b169851bca010ce5ca4
Sambhav Yadav <ry526270@gmail.com>
2026-08-20 12:17:57 +0530

    Time: 0 ms (100%), Space: 42.6 MB (48.6%) - LeetHub

--- 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.java
+++ 3492-maximum-containers-on-a-ship/3492-maximum-containers-on-a-ship.java
@@ -1,20 +1,7 @@
 class Solution {
     public int maxContainers(int n, int w, int maxWeight) {
-        int deck=n*n;
-        int container=0;
-        while(deck>0){
-            if(deck*w<=maxWeight){
-                container=deck;
-                break;
-             }
-            else{
-                deck--;
-             }
-
-        }
-
-        return container;
-        
+        int cell = n*n;
+        int weight = maxWeight / w;
+        return Math.min(cell,weight);
     }
-    
 }
