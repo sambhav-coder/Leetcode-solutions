a5428945839e2183e55086c458fc61cd04f66651
Sambhav Yadav <ry526270@gmail.com>
2026-08-26 12:20:37 +0530

    Time: 4 ms (78.46%), Space: 43.3 MB (60.41%) - LeetHub

---
+++ 1051-height-checker/1051-height-checker.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[]expected=heights.clone();
+        Arrays.sort(expected);
+        int c=0;
+        for(int i=0;i<heights.length;i++){
+            if(heights[i]!=expected[i]){
+                c++;
+            }
+        }
+        return c;
+        
+    }
+}
